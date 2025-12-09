91c5aa392862034dab19d9278be4836533c8508e
Leonardo Reiczak <leozak@gmail.com>
2025-12-09 13:25:24 -0300

    Hover ScheduleView item.

--- frontend/src/components/Schedule/ScheduleView.jsx
+++ frontend/src/components/Schedule/ScheduleView.jsx
@@ -1,11 +1,11 @@
 const ScheduleView = ({ todos }) => {
   return (
     <div>
-      <div>
+      <div className="inline-block">
         {todos.map((todo) => (
           <div
             key={todo.id}
-            className="flex rounded-md overflow-hidden border-red-700 border mb-1"
+            className="flex rounded-md overflow-hidden border-red-700 border mb-2 hover:bg-red-100 hover:cursor-pointer"
           >
             <div className="bg-red-700 w-2"></div>
             <div className="py-0.5 px-2">{todo.title}</div>
