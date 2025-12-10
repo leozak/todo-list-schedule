03ab506eb6be017357e5b4f9a226705251670c45
Leonardo Reiczak <leozak@gmail.com>
2025-12-10 15:52:02 -0300

    Melhorias nas telas de Cadastro e Login.

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,15 +1,132 @@
 import { useState } from "react";
 import { PiUserCircleFill } from "react-icons/pi";
 import { PiUserCirclePlusFill } from "react-icons/pi";
+import { FaUserEdit } from "react-icons/fa";
 
 const Login = () => {
-  const [username, setUsername] = useState("username");
-  const [password, setPassword] = useState("password");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [newUser, setNewUser] = useState(false);
+
+  const [errorName, setErrorName] = useState({
+    error: false,
+    message: "",
+  });
+  const [errorUsername, setErrorUsername] = useState({
+    error: false,
+    message: "",
+  });
+  const [errorPassword, setErrorPassword] = useState({
+    error: false,
+    message: "",
+  });
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+    if (e.target.value != "") {
+      setErrorName({ error: true, message: "" });
+    }
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+    if (e.target.value === "") {
+      setErrorUsername({ error: true, message: "Please enter a username." });
+    } else if (e.target.value.length < 6) {
+      setErrorUsername({
+        error: true,
+        message: "Username must be at least 6 characters long.",
+      });
+    } else {
+      setErrorUsername({ error: false, message: "" });
+    }
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value === "") {
+      setErrorPassword({ error: true, message: "Please enter a password." });
+    } else {
+      setErrorPassword({ error: false, message: "" });
+    }
+  };
+
+  const handleSignUp = () => {
+    let is_ok = true;
+    if (name === "") {
+      setErrorName({ error: true, message: "Please enter a name." });
+      is_ok = false;
+    } else {
+      setErrorName({ error: false, message: "" });
+    }
+
+    if (username === "") {
+      setErrorUsername({ error: true, message: "Please enter a username." });
+      is_ok = false;
+    } else if (username.length < 6) {
+      setErrorUsername({
+        error: true,
+        message: "Username must be at least 6 characters long.",
+      });
+      is_ok = false;
+    } else {
+      setErrorUsername({ error: false, message: "" });
+    }
+
+    if (password === "") {
+      setErrorPassword({ error: true, message: "Please enter a password." });
+      is_ok = false;
+    } else {
+      setErrorPassword({ error: false, message: "" });
+    }
+
+    //
+    // Verificar se o usuário ja existe
+    //
+
+    //
+    // Aqui colocar a lógica de cadastro
+    //
+
+    if (is_ok) {
+      setNewUser(false);
+    }
+  };
+
+  const handleSignIn = () => {
+    let is_ok = true;
+    if (username === "") {
+      setErrorUsername({ error: true, message: "Please enter a username." });
+      is_ok = false;
+    } else if (username.length < 6) {
+      setErrorUsername({
+        error: true,
+        message: "Username must be at least 6 characters long.",
+      });
+      is_ok = false;
+    } else {
+      setErrorUsername({ error: false, message: "" });
+    }
+
+    if (password === "") {
+      setErrorPassword({ error: true, message: "Please enter a password." });
+      is_ok = false;
+    } else {
+      setErrorPassword({ error: false, message: "" });
+    }
+
+    //
+    // Aqui colocar a lógica de login
+    //
+  };
 
   return (
     <>
       {!newUser ? (
+        //
+        // Login
+        //
         <div className="h-screen w-screen flex justify-center items-center">
           <div className="border-cyan-800 border w-md m-2 overflow-hidden rounded-3xl shadow-2xl">
             <div className="bg-cyan-800 h-5"></div>
@@ -21,31 +138,62 @@ const Login = () => {
 
             {/* Login title */}
             <h1 className="block text-3xl text-center font-bold text-gray-600">
-              Welcome to To-do List
+              Welcome Back!
             </h1>
+            <p className="text-center m-2">
+              Sign in to organize your life efficiently.
+            </p>
 
             {/* Login form */}
             <div className="p-6">
-              Username:
+              <label htmlFor="username">Username*</label>
               <input
+                id="username"
                 type="text"
-                className="w-full p-2"
+                value={username}
+                onChange={handleUsernameChange}
+                className="w-full p-2 border-gray-100 border rounded-xl"
                 placeholder="Enter your username"
               />
-              Password:
+              {errorUsername.error && (
+                <p className="text-red-500 text-xs mb-1 pl-2">
+                  {errorUsername.message}
+                </p>
+              )}
+
+              <label htmlFor="password">Password*</label>
               <input
+                id="password"
                 type="password"
-                className="w-full p-2"
+                value={password}
+                onChange={handlePasswordChange}
+                className="w-full p-2 border-gray-100 border rounded-xl"
                 placeholder="Enter your password"
               />
+              {errorPassword.error && (
+                <p className="text-red-500 text-xs mb-1 pl-2">
+                  {errorPassword.message}
+                </p>
+              )}
+
+              <button
+                onClick={handleSignIn}
+                className="w-full flex text-gray-200 bg-cyan-700 hover:bg-cyan-800 hover:cursor-pointer my-5 mt-6 rounded-xl p-2 items-center justify-center"
+              >
+                <FaUserEdit /> &nbsp; Sign in
+              </button>
             </div>
 
             <div className="text-center m-4">
+              Don't have an account? &nbsp;
               <button
-                onClick={() => setNewUser(true)}
-                className="hover:underline hover:text-cyan-600 hover:cursor-pointer"
+                onClick={() => {
+                  setNewUser(true);
+                  setPassword("");
+                }}
+                className="text-cyan-800 hover:underline hover:text-cyan-600 hover:cursor-pointer"
               >
-                Create new account.
+                Sign up
               </button>
             </div>
 
@@ -53,57 +201,89 @@ const Login = () => {
           </div>
         </div>
       ) : (
+        //
+        // New User
+        //
         <div className="h-screen w-screen flex justify-center items-center">
           <div className="border-cyan-800 border w-md m-2 overflow-hidden rounded-3xl shadow-2xl">
             <div className="bg-cyan-800 h-5"></div>
-
             {/* User Icon */}
             <div className="flex justify-center m-4">
               <PiUserCirclePlusFill className="h-50 w-50 text-cyan-700" />
             </div>
-
             {/* Login title */}
             <h1 className="block text-3xl text-center font-bold text-gray-600">
-              Welcome to To-do List
+              Create Your Account?
             </h1>
-
+            <p className="text-center m-2">
+              Create an account and optimize your life.
+            </p>
             {/* Login form */}
             <div className="p-6">
-              Username:
+              <label htmlFor="name">Full Name*</label>
               <input
+                id="name"
                 type="text"
-                className="w-full p-2"
-                placeholder="Enter your username"
-              />
-              Name:
-              <input
-                type="text"
-                className="w-full p-2"
+                value={name}
+                onChange={handleNameChange}
+                className="w-full p-2 border-gray-100 border rounded-xl"
                 placeholder="Enter your name"
               />
-              Password:
-              <input
-                type="password"
-                className="w-full p-2"
-                placeholder="Enter your password"
-              />
-              Re-password:
-              <input
-                type="password"
-                className="w-full p-2"
-                placeholder="Enter your password"
-              />
-            </div>
+              {errorName.error && (
+                <p className="text-red-500 text-xs mb-1 pl-2">
+                  {errorName.message}
+                </p>
+              )}
 
-            <div className="text-center m-4">
+              <label htmlFor="username">Username*</label>
+              <input
+                id="username"
+                type="text"
+                value={username}
+                onChange={handleUsernameChange}
+                className="w-full p-2 border-gray-100 border rounded-xl"
+                placeholder="Enter your username"
+              />
+              {errorUsername.error && (
+                <p className="text-red-500 text-xs mb-1 pl-2">
+                  {errorUsername.message}
+                </p>
+              )}
+
+              <label htmlFor="password">Password*</label>
+              <input
+                id="password"
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+                className="w-full p-2 border-gray-100 border rounded-xl"
+                placeholder="Enter your password"
+              />
+              {errorPassword.error && (
+                <p className="text-red-500 text-xs mb-1 pl-2">
+                  {errorPassword.message}
+                </p>
+              )}
+
               <button
-                onClick={() => setNewUser(false)}
-                className="hover:underline hover:text-cyan-600 hover:cursor-pointer"
+                onClick={handleSignUp}
+                className="w-full flex text-gray-200 bg-cyan-700 hover:bg-cyan-800 hover:cursor-pointer my-5 mt-6 rounded-xl p-2 items-center justify-center"
               >
-                Login as user.
+                <FaUserEdit /> &nbsp; Sign up
               </button>
             </div>
-
+            <div className="text-center m-4">
+              Already have an account? &nbsp;
+              <button
+                onClick={() => {
+                  setNewUser(false);
+                  setPassword("");
+                }}
+                className="text-cyan-800 hover:underline hover:text-cyan-600 hover:cursor-pointer"
+              >
+                Sign in
+              </button>
+            </div>
             <div className="bg-cyan-800 h-5"></div>
           </div>
         </div>
